b7d6b21d29a49dca974b59fff86f9f58727dba29
CaptainM <37575000+hlmiao@users.noreply.github.com>
2021-03-28 23:14:24 +0800

    Update and rename get_workflows_metadata to get_workflows_metadata.py

--- get_workflows_metadata.py
+++ get_workflows_metadata.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import pprint
 
-def get_resource_maetadata_of_workflows():
+def get_resource_metadata_of_workflows():
    session = boto3.session.Session()
    glue_client = session.client('glue')
    try:
@@ -11,11 +11,11 @@ def get_resource_maetadata_of_workflows():
 
       return list_of_workflows, response
    except ClientError as e:
-      raise Exception( "boto3 client error in get_resource_maetadata_of_workflows: " + e.__str__())
+      raise Exception( "boto3 client error in get_resource_metadata_of_workflows: " + e.__str__())
    except Exception as e:
-      raise Exception( "Unexpected error in get_resource_maetadata_of_workflows: " + e.__str__())
+      raise Exception( "Unexpected error in get_resource_metadata_of_workflows: " + e.__str__())
 
-a, b = get_resource_maetadata_of_workflows()
+a, b = get_resource_metadata_of_workflows()
 #List of Workflows
 pprint.pprint(a)
 print('\n')
